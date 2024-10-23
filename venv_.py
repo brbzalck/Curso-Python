b97e6a0ca5bc36593c2f1f6abdd440c3c4e06ccd
Lucas Barbosa <lukasbo9765@gmail.com>
2024-10-23 15:20:29 -0300

    Alterações venv

---
+++ venv_.py
@@ -0,0 +1,13 @@
+# VENV
+
+# python -m venv venv - para criar ambiente virtual
+
+# .\venv\Scripts\activate para ativar um ambiente venv
+# ou então venv\Scripts\activate
+# desactivate para desativar
+
+# pip é utilizado para instalar pacotes e blibliotecas
+# pip install django
+
+# pip freeze - para mostrar os pacotes e blibliotecas instaladas
+# pip freeze > requirements.txt - para salvar pacotes e blibliotecas utilizadas no projeto
