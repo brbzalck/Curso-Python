b29aea2522ce6b3a39e50beb6a66578866d0a6f4
Lucas Barbosa <lukasbo9765@gmail.com>
2024-11-22 14:55:07 -0300

    Classes & Heranças

--- pyside6/qapllication.py
+++ pyside6/qapllication.py
@@ -19,67 +19,61 @@ from PySide6.QtCore import Slot
 from PySide6.QtWidgets import (QApplication, QPushButton, QWidget, QGridLayout,
                                 QMainWindow)
 
+class MyWindow(QMainWindow):
+    def __init__(self, parent=None):
+        # Pegando o init da herança QMainWindow
+        super().__init__(parent)
 
-app = QApplication(sys.argv)
-window = QMainWindow()
-central_widget = QWidget()
-window.setCentralWidget(central_widget)
-window.setWindowTitle('Interface gráfica com PySide6')
+        self.central_widget = QWidget()
+        self.setCentralWidget(self.central_widget)
+        self.setWindowTitle('Interface gráfica com PySide6')
+
+        self.botao1 = self.make_button('Texto do botão')
+        self.botao1.clicked.connect(self.segunda_acao_marcada)
+        self.botao2 = self.make_button('Botão Dois')
+        self.botao3 = self.make_button('Botão Três')
+
+        self.grid_layout = QGridLayout()
+        self.central_widget.setLayout(self.grid_layout)
+
+        self.grid_layout.addWidget(self.botao1, 1, 1)
+        self.grid_layout.addWidget(self.botao2, 2, 1, 1, 2)
+        # 2=Linha, 1=Coluna, 1=spanLinha, 2=spanColuna
+        self.grid_layout.addWidget(self.botao3, 1, 2)
+
+        # staturBar
+        self.status_bar = self.statusBar()
+        self.status_bar.showMessage('Mostrar mensagem na barra')
+
+        # menuBar
+        self.menu = self.menuBar()
+        self.primeiro_menu = self.menu.addMenu('Primeiro menu')
+        self.primeira_acao = self.primeiro_menu.addAction('Primeira ação')
+        # Usando a lambda para executar a função slot_example
+        # com o argumento status_bar
+        self.primeira_acao.triggered.connect(self.muda_msg_sttsbar)
+
+        self.segunda_acao = self.primeiro_menu.addAction('Segunda ação')
+        self.segunda_acao.setCheckable(True)
+        self.segunda_acao.toggled.connect(self.segunda_acao_marcada)
+        self.segunda_acao.hovered.connect(self.segunda_acao_marcada)
+
+    @Slot()
+    def muda_msg_sttsbar(self):
+            self.status_bar.showMessage('O meu slot foi executado')
+
+    @Slot()
+    def segunda_acao_marcada(self):
+        print('Está marcado? ', self.segunda_acao.isChecked())
+
+    def make_button(self, text):
+        btn = QPushButton(text)
+        btn.setStyleSheet('font-size: 30px; color:green')
+        return btn
 
 
-botao = QPushButton('Texto do botão')
-botao.setStyleSheet('font-size: 30px; color:green')
-
-botao2 = QPushButton('Texto do botão2')
-botao.setStyleSheet('font-size: 30px; color:dark-cyan')
-
-botao3 = QPushButton('Texto do botão3')
-botao3.setStyleSheet('font-size: 30px; color:red')
-
-
-layout = QGridLayout()
-central_widget.setLayout(layout)
-
-layout.addWidget(botao, 1, 1)
-layout.addWidget(botao2, 2, 1, 1, 2)
-# 2=Linha, 1=Coluna, 1=spanLinha, 2=spanColuna
-layout.addWidget(botao3, 1, 2)
-
-@Slot()
-def slot_example(status_bar):
-    def inner():
-        status_bar.showMessage('O meu slot foi executado')
-    return inner
-
-
-@Slot()
-def outro_slot(checked):
-    print('Está marcado? ', checked)
-
-@Slot()
-def terceiro_slot(action):
-    def inner():
-        outro_slot(action.isChecked()) # Retorna um Bool 
-    return inner
-
-# staturBar
-status_bar = window.statusBar()
-status_bar.showMessage('Mostrar mensagem na barra')
-
-# menuBar
-menu = window.menuBar()
-primeiro_menu = menu.addMenu('Primeiro menu')
-primeira_acao = primeiro_menu.addAction('Primeira ação')
-# Usando a lambda para executar a função slot_example
-# com o argumento status_bar
-primeira_acao.triggered.connect(slot_example(status_bar))
-
-segunda_acao = primeiro_menu.addAction('Segunda ação')
-segunda_acao.setCheckable(True)
-segunda_acao.toggled.connect(outro_slot)
-segunda_acao.hovered.connect(terceiro_slot(segunda_acao))
-
-botao.clicked.connect(terceiro_slot(segunda_acao))
-
-window.show()
-app.exec()
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    window = MyWindow()
+    window.show()
+    app.exec()
