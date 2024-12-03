2d75e6fc29027e60d702d3ff477d3f228b5a19c6
Lucas Barbosa <lukasbo9765@gmail.com>
2024-12-03 16:22:58 -0300

    equal

--- calculadora-pyside6/buttons.py
+++ calculadora-pyside6/buttons.py
@@ -84,6 +84,11 @@ class ButtonsGrid(QGridLayout):
                 button,
                 self._makeSlot(self._operatorClicked, button)
                   )
+            
+        if text in '=':
+
+            self._connectButtonClicked(button, self._eq)
+
 
     def _makeSlot(self, func, *args, **kwargs):
         @Slot(bool)
@@ -123,3 +128,24 @@ class ButtonsGrid(QGridLayout):
 
         self._op = buttonText
         self.equation = f'{self._left} {self._op} ??'
+
+    def _eq(self):
+        displayText = self.display.text()
+
+        if not isValidNumber(displayText):
+            print('Sem nada para a direita')
+            return
+        
+        self._right = float(displayText)
+        self.equation = f'{self._left} {self._op} {self._right}'
+        result = 0.0
+
+        try:
+            result = eval(self.equation)
+        except ZeroDivisionError:
+            print('ZeroDivisionError')
+
+        self.display.clear()
+        self.info.setText(f'{self.equation} = {result}')
+        self._left = result
+        self._right = None
