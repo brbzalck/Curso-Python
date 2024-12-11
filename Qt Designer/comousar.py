175aa1f0de7bdc4093da461745e987cab1ef90c0
Lucas Barbosa <lukasbo9765@gmail.com>
2024-12-11 11:26:27 -0300

    Qt Designer

---
+++ Qt Designer/comousar.py
@@ -0,0 +1,4 @@
+# pyside6-uic '.\Qt Designer\ui\window.ui' -o '.\Qt Designer\src\window.py'
+# pyside6-uic - para compilar as informações ui para python
+# Caminho do arquivo ui
+# -o +Caminho do arquivo py, para especificar aonde irá salvar
