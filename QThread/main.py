cd191f241df858bdc1b19b99cb52df57e5110481
Lucas Barbosa <lukasbo9765@gmail.com>
2024-12-12 11:11:35 -0300

    QObject e QThread: criando o "Worker"

--- QThread/main.py
+++ QThread/main.py
@@ -1,8 +1,23 @@
+from PySide6.QtCore import QObject, Signal ,Slot
 from PySide6.QtWidgets import QApplication, QWidget
 from ui_workerui import Ui_myWidget
 import sys
 import time
 
+
+class Worker1(QObject):
+    started = Signal(str)
+    progressed = Signal(str)
+    finished = Signal(str)
+
+    def run(self):
+        value = '0'
+        self.started.emit(value)
+        for i in range(5):
+            value = str(i)
+            self.progressed.emit(value)
+            time.sleep(1)
+        self.finished.emit(value)
 
 class MyWidget(QWidget, Ui_myWidget):
     def __init__(self, parent=None):
@@ -13,9 +28,6 @@ class MyWidget(QWidget, Ui_myWidget):
         self.button2.clicked.connect(self.hardWorker2)
 
     def hardWorker1(self):
-        for i in range(5):
-            print(i)
-            time.sleep(1)
         self.label1.setText('1 terminado')
 
     def hardWorker2(self):
