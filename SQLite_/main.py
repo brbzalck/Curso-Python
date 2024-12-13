4ba99044abe848b40d2134213626b242f6144d23
Lucas Barbosa <lukasbo9765@gmail.com>
2024-12-13 09:54:37 -0300

    Criando minha primeira tabela no SQLite3

--- SQLite_/main.py
+++ SQLite_/main.py
@@ -4,12 +4,22 @@ from pathlib import Path
 ROOT_DIR = Path(__file__).parent
 DB_NAME = 'db.sqlite3'
 DB_FILE = ROOT_DIR / DB_NAME
+TABLE_NAME = 'customers'
 
 
 con = sqlite3.connect(DB_FILE)
 cursor = con.cursor()
 
-# SQL
+cursor.execute(
+    f'CREATE TABLE IF NOT EXISTS {TABLE_NAME}'
+    '('
+    #
+    'id INTEGER PRIMARY KEY AUTOINCREMENT,'
+    'name TEXT,'
+    'weight REAL'
+    ')'
+)
+con.commit()
 
 cursor.close()
 con.close()
