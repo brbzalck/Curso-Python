06cce13db43bc54af9af8bde2480c7e22409f05a
Lucas Barbosa <lukasbo9765@gmail.com>
2024-12-13 10:10:46 -0300

    Inserindo valores (INSERT INTO), DELETE sem WHERE e zerando a sqlite_sequence

--- SQLite_/main.py
+++ SQLite_/main.py
@@ -10,16 +10,34 @@ TABLE_NAME = 'customers'
 con = sqlite3.connect(DB_FILE)
 cursor = con.cursor()
 
+#  CUIDADO!: fazendo delete sem where
+cursor.execute(
+    f'DELETE FROM {TABLE_NAME}'
+)
+cursor.execute(
+    f'DELETE FROM sqlite_sequence WHERE name="{TABLE_NAME}"'
+)
+con.commit()
+#  Cria a tabela
 cursor.execute(
     f'CREATE TABLE IF NOT EXISTS {TABLE_NAME}'
     '('
-    #
     'id INTEGER PRIMARY KEY AUTOINCREMENT,'
     'name TEXT,'
     'weight REAL'
     ')'
 )
 con.commit()
+
+#  Registrar valores nas colunas das tabelas
+#  CUIDADO: sql injection
+cursor.execute(
+    f'INSERT INTO {TABLE_NAME} (id, name, weight) '
+    'VALUES (NULL, "Lucas Barbosa", 9.9), (NULL, "João Vitor", 16)'
+)
+con.commit()
+
+
 
 cursor.close()
 con.close()
