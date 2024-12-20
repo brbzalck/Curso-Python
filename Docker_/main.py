23481f9dd0552c5f4946b00d06ad5b2bf2ac9cf9
Lucas Barbosa <lukasbo9765@gmail.com>
2024-12-20 10:41:42 -0300

    Usando python-dotenv e .env com pymysql.connect

--- Docker_/main.py
+++ Docker_/main.py
@@ -3,18 +3,21 @@
 # GitHub: https://github.com/PyMySQL/PyMySQL
 
 import pymysql
+import dotenv
+import os
+
+dotenv.load_dotenv()
 
 connection = pymysql.connect(
-    host='localhost',
+    host=os.environ['MYSQL_HOST'],
     #  Especificando a porta que joga p dentro do container
-    port=3307,
-    user='usuario',
-    password='senha',
-    database='base_de_dados',
+    port=int(os.environ['MYSQL_PORT']),
+    user=os.environ['MYSQL_USER'],
+    password=os.environ['MYSQL_PASSWORD'],
+    database=os.environ['MYSQL_DATABASE'],
 )
 
 #  Usando context manager par fechar automaticamente a conexão:
 with connection:
      with connection.cursor() as cursor:
           print(cursor)
-          
