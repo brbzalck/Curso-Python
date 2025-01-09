53c10a70e577557c4c4f5e5610837caf98218b5c
Lucas Barbosa <lukasbo9765@gmail.com>
2025-01-09 14:36:32 -0300

    DELETE

---
+++ Scripts_MySql/delete.sql
@@ -0,0 +1,7 @@
+-- Delete apaga registros da tabela
+
+-- use para delete a PK de preferência
+DELETE FROM users WHERE id=80;
+
+-- SEMPRE use WHERE junto com DELETE
+select * FROM users WHERE id BETWEEN 50 and 100;
